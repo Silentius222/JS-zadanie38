5c7fe019f2ea0bf5853dcc1e5ab32b5b6265b6a1
Silentius222 <79799875+Silentius222@users.noreply.github.com>
2022-06-15 11:15:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 function funkcja(a, b){
     let dodawanie = a + b;
-    console.log = ("Wynik dodawania wynosi: ") + dodawanie;
-    let odejmowanie = a - b;
-    console.log = ("Wynik odejmowania wynosi: ") + odejmowanie;
-    let mnozenie = a * b;
-    console.log = ("Wynik mnozeniawynosi: ") + mnozenie;
+    if (dodawanie > 0) {
+    console.log("Wynik dodawania wynosi: " + dodawanie)
+  } else {
+    console.log("Wynik jest nieprawidłowy")
+  }
 
-    if ((dodawanie || odejmowanie || mnozenie) < 0){
-        console.log = "Wynik jest nieprawidłowy";
-    }
+    let odejmowanie = a - b;
+    if (odejmowanie > 0) {
+    console.log("Wynik odejmowania wynosi: " + odejmowanie)
+  } else {
+    console.log("Wynik jest nieprawidłowy")
+  }
+
+    let mnozenie = a * b;
+    if (mnozenie > 0) {
+    console.log("Wynik mnozenia wynosi: " + mnozenie)
+  } else {
+    console.log("Wynik jest nieprawidłowy")
+  }
+
 }
